8e662e28cc6c8382f3b560a3dcd9e57287ae10e4
AjileJ <53097022+AjileJ@users.noreply.github.com>
2020-08-04 20:51:06 -0700

    frontend pretty much complete

---
+++ blog/src/pages/AboutPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const AboutPage = () => (
+    <>
+    <h1>About Me</h1>
+
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum vel, fuga nisi ea numquam autem veritatis ut illo cupiditate dignissimos saepe sint nihil aspernatur consequatur! Doloremque minima pariatur dolorum labore?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis molestias numquam accusantium est fugiat consectetur minima voluptates nulla consequuntur excepturi sint eum totam quibusdam ad in ea odit, libero cum.</p>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum fuga expedita repudiandae et quae, at impedit dolore architecto fugit! Quas, illum odit ipsam voluptas nulla quidem molestias! Quo, illum magni. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae, quasi consectetur laborum repudiandae quaerat at magni sapiente debitis corporis odio dolore dolores ipsam aliquam? Tempora vero eligendi cum in sed?</p>
+    
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi quasi ducimus quia quis tempore repellendus obcaecati similique, reprehenderit voluptatibus harum modi facilis illo? Aut aliquid eligendi cum dolore voluptatem quam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio magnam maiores accusamus perferendis dolorem suscipit, at ullam illum aut autem laborum ratione placeat optio blanditiis sint sed quo eum eaque.</p>
+    </>
+    )
+
+
+export default AboutPage
